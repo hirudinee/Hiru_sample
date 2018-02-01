33610c37f43926ea156a492141bbd94b765461c8
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-01 09:53:08 +0530

    db commit

---
+++ Hiru_sample/lambda.js
@@ -0,0 +1,19 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+
+	ddb.put({
+		TableName: 'Hiru_Test',
+		Item: { 'sample': '001' }
+	}, function (err, data) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(data);
+		}
+	});
+
+
+
+	callback(null, 'Successfully executed');
+}
